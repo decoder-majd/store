cf660b6a4c9624dd1820d4b957fae08a6a1ab65e
majd Khasib <majd20171998@gmail.com>
2019-09-20 18:57:42 +0300

    Project is done without css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {RouterModule} from '@angular/router';
+import {ProductModel} from './product.model';
+import { ProductAlertsComponent } from './product-alerts/product-alerts/product-alerts.component';
+import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ToolbarComponent,
+    ProductAlertsComponent,
+    ProductDetailComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent}
+    ]),
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ProductModel],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
